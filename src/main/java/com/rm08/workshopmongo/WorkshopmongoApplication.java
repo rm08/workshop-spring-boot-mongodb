5d1779888bd740c954e26ede551b9f8e01ffa34b
rm08 <rafael.massinatore@gmail.com>
2022-05-05 15:46:13 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/rm08/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/rm08/workshopmongo/WorkshopmongoApplication.java
@@ -2,8 +2,10 @@ package com.rm08.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.rm08.workshopmongo;")
 public class WorkshopmongoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/rm08/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.rm08.workshopmongo.repository;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rm08.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
